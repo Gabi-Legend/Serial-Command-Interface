2ff3a75afd6ab977330a8f39c1de0acacf9c3c23
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-02-01 19:29:46 +0200

    defining variables

--- serial_command_interface.ino
+++ serial_command_interface.ino
@@ -1,8 +1,21 @@
+#include <Arduino.h>
+
+struct SystemState {
+  unsigned long startTime;
+  int mode;
+  bool debug;
+};
+
+SystemState sys;       
+char cmdBuffer[32];        
+uint8_t cmdIndex = 0;   
+bool cmdReady = false;    
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("First output!");
+  sys.startTime = millis();
+  Serial.println("Mini-OS ready. Type 'help'.");
 }
 
 void loop() {
-
 }
